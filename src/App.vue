e4b82d41f3226079997279cce8560a5dbc96e760
Quyen Thien <phamquyenthien@gmail.com>
2022-10-31 22:11:23 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+
+  }
+</script>
+
+npm run dev
+
+<template>
+  <h1>Hello, vue.js!</h1>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+  .page {
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
